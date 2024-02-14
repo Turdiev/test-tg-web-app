3651da637a3b4e14a70f5b77d3ed2e697806c38e
Turdiev <turdiev@qtim.ru>
2024-02-14 19:50:12 +0300

    page fix

--- src/widgets/Support/ui/SupportFaq/SupportFaq.vue
+++ src/widgets/Support/ui/SupportFaq/SupportFaq.vue
@@ -4,7 +4,7 @@ import {TitleH3} from '@/shared/ui/title';
 </script>
 
 <template>
-  <router-link to="/support-faq" class="support-faq">
+  <router-link to="/support/faq" class="support-faq">
     <div class="support-faq__wrapper">
       <title-h3>FAQ</title-h3>
       <span class="support-faq__text">Часто задаваемые вопросы</span>

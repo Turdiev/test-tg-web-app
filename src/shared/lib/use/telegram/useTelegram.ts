ca14f1b296b38e9348c7c446841b0683a6eb0b3e
Turdiev <turdiev@qtim.ru>
2024-03-11 11:12:37 +0300

    initData

--- src/shared/lib/use/telegram/useTelegram.ts
+++ src/shared/lib/use/telegram/useTelegram.ts
@@ -1,7 +1,7 @@
 
 export function useTelegram() {
     const webApp = window.Telegram.WebApp
-    const initData = window.Telegram.WebApp.initData
+    const initData = webApp.initData
     const onClose = () => {
         console.log('close')
         tg.close()

68fa7507e91cdc0c4a964d0277bd12d7e19312cb
Turdiev <turdiev@qtim.ru>
2024-04-10 17:02:28 +0300

    viewportChanged

--- src/entities/Balance/ui/BalanceDetails/BalanceDetails.vue
+++ src/entities/Balance/ui/BalanceDetails/BalanceDetails.vue
@@ -4,11 +4,29 @@ import {useProfileStore} from "@/entities/Balance/model";
 import {storeToRefs} from "pinia";
 import {CurrencyType} from "@/shared/types";
 import {useTelegram} from "@/shared/lib/use";
+import {onMounted, watch} from "vue";
 
-const { viewportHeightTelegram, viewportStableHeightTelegram  } = useTelegram()
+const { webApp, viewportHeightTelegram, viewportStableHeightTelegram  } = useTelegram()
 
 const balanceStorage = useProfileStore()
 const { profileInfo } = storeToRefs(balanceStorage)
+
+watch([viewportHeightTelegram, viewportStableHeightTelegram], (val1, val2) => {
+  console.log('change')
+  webApp.onEvent('viewportChanged', (event) => {
+    console.log('1viewportChanged', event)
+    console.log('2viewportHeightTelegram', viewportHeightTelegram)
+    console.log('3viewportStableHeightTelegram', viewportStableHeightTelegram)
+  })
+})
+
+onMounted(() => {
+  webApp.onEvent('viewportChanged', (event) => {
+    console.log('viewportChanged', event)
+    console.log('viewportHeightTelegram', viewportHeightTelegram)
+    console.log('viewportStableHeightTelegram', viewportStableHeightTelegram)
+  })
+})
 </script>
 
 <template>
